615920d107489ab8450a4407d8f1f69da843304b
Tamas Unyatinszki <unyatinszkitamas@gmail.com>
2022-05-01 21:32:25 +0200

    Adding initial Spring Boot project

---
+++ src/main/java/unyat/salgot/SalgotApplication.java
@@ -0,0 +1,13 @@
+package unyat.salgot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalgotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalgotApplication.class, args);
+	}
+
+}
